98214c20d3abbea18c3a8f5e9b798c991def5d27
destroylord <masapin68@gmail.com>
2024-10-03 13:32:46 +0700

    fcmmit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit earum aliquam corporis ex mollitia harum cupiditate, sequi a itaque tenetur deserunt, voluptatem quaerat, magni inventore quisquam beatae et aperiam rerum!
+            </p>
+        </div>
+    );
+}
